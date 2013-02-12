9c872bc909bb52a7066bddc6ece00cd4a3fcfc1c
Samuraikurisu <chilgenb@gmail.com>
2013-02-12 12:22:13 -0800

    Create protos.h

---
+++ protos.h
@@ -0,0 +1,25 @@
+//Chris Hilgenberg
+//Lab 6&7
+//2-22-12
+//Function Prototypes
+
+
+//void SendCommand(char*);
+void SendData(char*);
+void Delay(long);
+void InitializeLCDIO();
+void InitializeDisplay();
+void Toggle();
+void Read();
+void Write();
+void Command();
+void Data();
+void ResetDisplay();
+void SecondLine();
+void BusyCheck();
+void ReadK();
+void WriteK();
+void ELOW();
+void EHIGH();
+void ReadFromLCD();
+void CheckSwitches();
